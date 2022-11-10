441f2a4fe54b43b3d948ba0feefd64200539112d
田代智 <r03.web.2110258@da.iwasaki.ac.jp>
2022-11-10 17:13:51 +0900

    login check機能実装

--- infrastructure/Routing.go
+++ infrastructure/Routing.go
@@ -3,6 +3,8 @@ package infrastructure
 import (
 	"github.com/gin-gonic/gin"
 
+	"github.com/set2002satoshi/8-4/pkg/module/service/authentication/auth"
+
 	uc "github.com/set2002satoshi/8-4/interfaces/controllers/user"
 	bc "github.com/set2002satoshi/8-4/interfaces/controllers/blog"
 )
@@ -24,26 +26,37 @@ func NewRouting(db *DB) *Routing {
 }
 
 func (r *Routing) setRouting() {
-	user := r.Gin.Group("/api")
-	{
-		usersController := uc.NewUsersController(r.DB)
-		user.GET("/users", func(c *gin.Context) { usersController.FindAll(c) })
-		user.PUT("/users", func(c *gin.Context) { usersController.FindByID(c) })
-		user.POST("/users", func(c *gin.Context) { usersController.Create(c) })
-		user.POST("/users/update", func(c *gin.Context) { usersController.Update(c)} )
-		user.DELETE("/users", func(c *gin.Context) { usersController.Delete(c) })
 
-		user.POST("/auth/login", func(c *gin.Context) { usersController.Login(c)})
+	usersController := uc.NewUsersController(r.DB)
+	blogsController := bc.NewBlogsController(r.DB)
+	
+	userNotLoggedIn := r.Gin.Group("/api")
+	{	
+		// auth
+		userNotLoggedIn.POST("/auth/login", func(c *gin.Context) { usersController.Login(c)})
+		
+		// user
+		userNotLoggedIn.GET("/users", func(c *gin.Context) { usersController.FindAll(c) })
+		userNotLoggedIn.PUT("/users", func(c *gin.Context) { usersController.FindByID(c) })
+		
+		// blog
+		userNotLoggedIn.GET("/blogs", func(c *gin.Context) {blogsController.Find(c)})
+		userNotLoggedIn.PUT("/blog", func(c *gin.Context) { blogsController.FindByID(c)})
 	}
 
-	blog := r.Gin.Group("/api")
-	{	
-		blogsController := bc.NewBlogsController(r.DB)
-		blog.GET("/blogs", func(c *gin.Context) {blogsController.Find(c)})
-		blog.PUT("/blog", func(c *gin.Context) { blogsController.FindByID(c)})
-		blog.POST("/blog", func (c *gin.Context) { blogsController.Create(c)})
-		blog.POST("/blog/update", func (c *gin.Context) { blogsController.Update(c)})
-		blog.DELETE("/blog", func(c *gin.Context) { blogsController.Delete(c)})
+	userLoggedIn := r.Gin.Group("/api")
+	userLoggedIn.Use(auth.CheckLoggedIn())
+	{
+
+		// user 
+		userLoggedIn.POST("/users", func(c *gin.Context) { usersController.Create(c) })
+		userLoggedIn.POST("/users/update", func(c *gin.Context) { usersController.Update(c)} )
+		userLoggedIn.DELETE("/users", func(c *gin.Context) { usersController.Delete(c) })
+
+		// blog
+		userLoggedIn.POST("/blog", func (c *gin.Context) { blogsController.Create(c)})
+		userLoggedIn.POST("/blog/update", func (c *gin.Context) { blogsController.Update(c)})
+		userLoggedIn.DELETE("/blog", func(c *gin.Context) { blogsController.Delete(c)})
 	}
 }
 
